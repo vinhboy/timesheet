e35f6b8f76ddd62acf9a92e92c1243efd734b4b4
Vinh Pham <vinhboy@gmail.com>
2018-04-12 12:10:20 -0700

    change root path and create Sheet model methods

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -1,3 +1,11 @@
 class Sheet < ApplicationRecord
     has_many :line_items
+
+    def total_time
+        line_items.sum(:minutes)
+    end
+
+    def total_cost
+        line_items.sum(:minutes) * rate
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :sheets do
     resources :line_items
   end
+
+  root 'sheets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
